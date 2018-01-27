46bacf2a7161967e732f1622c13e0c470a9ca08c
Lukasz <lredzinski@gmail.com>
2018-01-27 12:44:48 +0100

    added responsive menu

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,23 @@
 $(function() {
 
+$(window).scroll(function() {
+  	if($(document).scrollTop() > 50) {
+    	$('#nav').addClass('shrink');
+    }
+    else {
+    $('#nav').removeClass('shrink');
+    }
+  });
+
+
+
+
 	$('body').scrollspy({ target: '#navigation-bar' });
 
-
-$('#designControls').on('slid.bs.carousel', function() {
-		var currentSlideName = $('#design-id');
-		var activeSlideNumber = $('div.active').data('number');
-		currentSlideName.text(activeSlideNumber);
+	$('#designControls').on('slid.bs.carousel', function() {
+			var currentSlideName = $('#design-id');
+			var activeSlideNumber = $('div.active').data('number');
+			currentSlideName.text(activeSlideNumber);
 	});
 
 });
